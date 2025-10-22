e79eb5eab0383c275fb2462289699faa78324b75
NoSoyVerde <hectorkotor@gmail.com>
2025-10-22 10:15:28 +0200

    Proyecto acabado, quiza añadire mas cosas

--- jfernandez/js/config.example.js
+++
@@ -1,7 +0,0 @@
-// Ejemplo de `config.js` para uso local
-// Copia este archivo a `config.js` y reemplaza la apiKey por la tuya.
-// No subas `config.js` al repositorio.
-window.SR = window.SR || {};
-window.SR.config = window.SR.config || {
-  apiKey: 'TU_API_KEY_AQUI'
-};
